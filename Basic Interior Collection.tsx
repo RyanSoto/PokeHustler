686290eaff8a7fc2ae6101e84dca4413bf792bb5
RyanSoto <111201647+RyanSoto@users.noreply.github.com>
2022-08-13 15:43:47 -0500

    Add files via upload

---
+++ Basic Interior Collection.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Basic Interior Collection" tilewidth="16" tileheight="16" tilecount="520" columns="8">
+ <image source="../../../Downloads/Ekats Public Tiles/Ekat's Public Tiles/Basic Interior Collection.png" width="128" height="1040"/>
+</tileset>
